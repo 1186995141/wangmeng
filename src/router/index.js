d6eaccc6f9e6842d42ca3e3be4c43d2381cbfdae
wangmeng <1186995141@qq.com>
2020-05-05 01:14:55 +0800

    构建项目，配置全局样式，路由

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+   redirect:"login" //重定向
+  },
+  {
+   path:"/login",
+   name:"login",
+   component:()=>import("../views/login/index")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
